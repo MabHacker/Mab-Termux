7a0dfab85233a66c9eb2e517eab01826d95f5a65
Azhar Baig <30386283+MabHacker@users.noreply.github.com>
2020-04-12 00:46:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ rm bash.bashrc motd
 cd /data/data/com.termux/files/home/Mab-Termux
 cp bash.bashrc motd /data/data/com.termux/files/usr/etc
 cd
-rm -r Mab-Termux
-apt update
+apt update -y
+apt upgrade -y
 
 
 
